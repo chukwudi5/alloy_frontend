85159336466075c882d5a44662e54bd21fd9a416
chukwudi5 <stanleyonwumelu@gmail.com>
2023-09-07 12:15:12 -0400

    Create form and add CSS for initial application input

--- src/App.js
+++ src/App.js
@@ -4,20 +4,68 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+          <legend>Name</legend>
+          <input
+            type='text'
+            name='fname'
+          />
+          <label for='fname'>First Name</label>
+          <input
+            type='text'
+            name='lname'
+          />
+          <label for='lname'>Last Name</label>
+        </fieldset>
+        <fieldset>
+          <legend>Address</legend>
+          <input
+            type='text'
+            name='line1'
+          />
+          <label for='line1'>Street Address</label>
+          <input
+            type='text'
+            name='city'
+          />
+          <label for='city'>City</label>
+          <input
+            type='text'
+            name='state'
+          />
+          <label for='state'>State</label>
+          <input
+            type='text'
+            name='zip'
+          />
+          <label for='zip'>Postal/Zip Code</label>
+          <input
+            type='text'
+            name='country'
+          />
+          <label for='country'>Country</label>
+        </fieldset>
+        <fieldset>
+          <legend>Personal</legend>
+          <input
+            type='text'
+            name='ssn'
+          />
+          <label for='ssn'>Social Security Number</label>
+          <input
+            type='email'
+            name='email'
+          />
+          <label for='email'>Email</label>
+          <input
+            type='text'
+            name='birth'
+          />
+          <label for='birth'>Date of Birth</label>
+        </fieldset>
+      </form>
+      
     </div>
   );
 }
